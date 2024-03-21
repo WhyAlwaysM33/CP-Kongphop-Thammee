87c1a014b17870677362273d550643e375abe86b
WhyAlwaysM33 <163137653+WhyAlwaysM33@users.noreply.github.com>
2024-03-21 19:55:08 +0700

    Create Exercise4_Kongphop_Th.py
    create scoreboard

---
+++ Exercise4_Kongphop_Th.py
@@ -0,0 +1,11 @@
+eng = 88.8
+bus = 77.0
+com = 80.0
+prog = 100.0
+
+print("               Your Score                ")
+print("-----------------------------------------")
+print("Foundation English               :",eng)
+print("General Business                 :",bus)
+print("Introduction to Computer Systems :",com)
+print("Computer Programming             :",prog)
